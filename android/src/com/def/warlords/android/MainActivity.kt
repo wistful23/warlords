25a77ed8c6be04f95cb46c6b17083adba10735ae
Dmitry Filippov <wistful23@gmail.com>
2025-04-22 12:42:55 -0400

    Always keep screen on

--- android/src/com/def/warlords/android/MainActivity.kt
+++ android/src/com/def/warlords/android/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.def.warlords.android
 
 import android.os.Bundle
+import android.view.WindowManager
 import androidx.activity.ComponentActivity
 import androidx.core.view.WindowCompat
 import androidx.core.view.WindowInsetsCompat
@@ -15,6 +16,7 @@ class MainActivity : ComponentActivity() {
         val insetsController = WindowCompat.getInsetsController(window, window.decorView)
         insetsController.hide(WindowInsetsCompat.Type.systemBars())
         insetsController.systemBarsBehavior = WindowInsetsControllerCompat.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
+        window.addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON)
         // Create the main view.
         val contentView = MainView(this)
         setContentView(contentView)
